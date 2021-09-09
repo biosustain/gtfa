a0cf8c9f23c9387da491af5fbea998553566f481
Jason Jooste <jason.jooste@campus.lmu.de>
2021-09-09 11:01:53 +0200

    Refactor testing and add tests for excluded reactions

---
+++ src/tests/model_setup.py
@@ -0,0 +1,125 @@
+import logging
+import shutil
+import sys
+from pathlib import Path
+
+import pandas as pd
+import pytest
+from cobra import Model, Metabolite, Reaction
+from cobra.io import load_model
+from multitfa.core import tmodel
+
+from src import model_conversion
+
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+@pytest.fixture
+def ecoli_model():
+    # Make sure that logging print statements still work
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    # Write the files
+    tmodel = build_test_model()
+    test_dir = Path("test_dir")
+    if test_dir.exists():
+        shutil.rmtree(test_dir)
+    result_dir = test_dir / "results"
+    result_dir.mkdir(parents=True)
+    model_conversion.write_model_files(tmodel, test_dir)
+    yield tmodel
+    # Clean up
+    shutil.rmtree(test_dir)
+
+
+@pytest.fixture
+def model_small():
+    # Make sure that logging print statements still work
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    # Write the files
+    tmodel = build_small_test_model()
+    test_dir = Path("test_dir")
+    result_dir = test_dir / "results"
+    if test_dir.exists():
+        shutil.rmtree(test_dir)
+    test_dir.mkdir()
+    result_dir.mkdir()
+    model_conversion.write_model_files(tmodel, test_dir)
+    # We need at least one measurment
+    header = pd.DataFrame(columns=["measurement_type","target_id","condition_id","measurement","error_scale"],
+                          data=[["mic", "f6p_c", "condition_1", 1.2, 0.1]])
+    header.to_csv(test_dir / "measurements.csv", index=False)
+    yield tmodel
+    # Clean up
+    shutil.rmtree(test_dir)
+
+def build_small_test_model():
+    """
+    Make the small toy model with the interconversion between F6P,
+    :return:
+    """
+    model = Model("small_toy")
+    model.add_metabolites([
+        Metabolite(id="f6p_c", name="Fructose 6 phosphate", compartment="c"),
+        Metabolite(id="g6p_c", name="Glucose 6 phosphate", compartment="c"),
+        Metabolite(id="f1p_c", name="Fructose 1 phosphate", compartment="c"),
+        Metabolite(id="g1p_c", name="Glucose 1 phosphate", compartment="c")
+    ])
+    model.add_reactions([Reaction("g6p/g1p"),
+                         Reaction("g1p/f1p"),
+                         Reaction("f1p/f6p"),
+                         Reaction("f6p/g6p"),
+                         Reaction("f6p/g1p"),
+                         ])
+    # Add the reactions
+    # Add the stoichiometry
+    model.reactions[0].build_reaction_from_string("g6p_c <--> g1p_c"),
+    model.reactions[1].build_reaction_from_string("g1p_c <--> f1p_c"),
+    model.reactions[2].build_reaction_from_string("f1p_c <--> f6p_c"),
+    model.reactions[3].build_reaction_from_string("f6p_c <--> g6p_c"),
+    model.reactions[4].build_reaction_from_string("f6p_c <--> g1p_c")
+    # Boundary reactions
+    model.add_boundary(model.metabolites.get_by_id("g6p_c"), type="sink", lb=1, ub=1)
+    model.add_boundary(model.metabolites.get_by_id("f1p_c"), type="exchange")
+    # Make the compartment info
+    compartment_info = pd.DataFrame([[7.5, 0.25, 298.15]], index=["c"], columns=["pH", "I", "T"])
+    thermo_model = tmodel(model, compartment_info=compartment_info)
+    # Add the KEGG ids
+    thermo_model.metabolites[0].Kegg_id = "kegg:C00085"
+    thermo_model.metabolites[1].Kegg_id = "kegg:C00668"
+    thermo_model.metabolites[2].Kegg_id = "kegg:C05001"
+    thermo_model.metabolites[3].Kegg_id = "kegg:C00103"
+
+    return thermo_model
+
+####### Taken directly from multitfa load_test_model
+def build_test_model():
+    model = load_model("e_coli_core")
+    pH_I_T_dict = {
+        "pH": {"c": 7.5, "e": 7, "p": 7},
+        "I": {"c": 0.25, "e": 0, "p": 0},
+        "T": {"c": 298.15, "e": 298.15, "p": 298.15},
+    }
+    del_psi_dict = {
+        "c": {"c": 0, "e": 0, "p": 150},
+        "e": {"c": 0, "e": 0, "p": 0},
+        "p": {"c": -150, "e": 0, "p": 0},
+    }
+    del_psi = pd.DataFrame.from_dict(data=del_psi_dict)
+    comp_info = pd.DataFrame.from_dict(data=pH_I_T_dict)
+    Excl = [rxn.id for rxn in model.boundary] + [
+        "BIOMASS_Ecoli_core_w_GAM",
+        "O2t",
+        "H2Ot",
+    ]
+    tfa_model = tmodel(
+        model, Exclude_list=Excl, compartment_info=comp_info, membrane_potential=del_psi
+    )
+    for met in tfa_model.metabolites:
+        kegg_id = "bigg.metabolite:" + met.id[:-2]
+        met.Kegg_id = kegg_id
+    tfa_model.update()
+    return tfa_model
+########## End of direct copy
---
+++ src/tests/test_assorted.py
@@ -0,0 +1,84 @@
+from pathlib import Path
+
+import numpy as np
+import pytest
+import scipy
+
+import src.util as util
+from src.fitting import stan_input_from_dir
+from src.model_conversion import write_model_files
+from model_setup import ecoli_model, model_small
+
+def test_get_free_fluxes_solution_many():
+    """ Test that the free and calculated functions satisfy the steady state equation"""
+    total_rows = 20
+    total_cols = 100
+
+    for nrows in range(3, total_rows):
+        for ncols in range(nrows+1, total_cols):
+            random_matrix = (scipy.sparse.rand(nrows, ncols, density=0.3, random_state=nrows+ncols).toarray() - 0.5) * 3
+            random_matrix = np.around(random_matrix)
+            get_free_fluxes_solution(random_matrix)
+
+
+def get_free_fluxes_solution(random_matrix):
+    """ Test that random free fluxes and calculated determined fluxes give the a steady state soltuion"""
+    repeats = 10
+    eps = 1e-8
+    nrows, ncols = random_matrix.shape
+    free_cols, fixed_mat = util.get_free_fluxes(random_matrix)
+    for repeat in range(repeats):
+        v = np.zeros(ncols)
+        random_v = np.random.randn(sum(free_cols))
+        v[free_cols] = random_v
+        # Now assign the values to the rest of the fluxes
+        fixed_fluxes = fixed_mat @ random_v
+        v[~free_cols] = fixed_fluxes
+        assert all(abs(random_matrix @ v) < eps)
+
+
+@pytest.mark.usefixtures("model_small")
+def test_excluded_reactions_single(model_small):
+    # Add the test dir
+    test_dir = Path("test_dir")
+    # Test first without the new excluded reaction
+    stan_input = stan_input_from_dir(test_dir)
+    assert stan_input["N_transport"] == 2, "Standard transport reaciton"
+    model_small.Exclude_list = ["g6p/g1p"]
+    # Write the files again
+    write_model_files(model_small, test_dir)
+    stan_input = stan_input_from_dir(test_dir)
+    # Test the expected input
+    assert stan_input["N_transport"] == 3, "Expect extra transport reaction"
+
+
+@pytest.mark.usefixtures("model_small")
+def test_excluded_reactions_double(model_small):
+    # Add the test dir
+    test_dir = Path("test_dir")
+    # Test first without the new excluded reaction
+    stan_input = stan_input_from_dir(test_dir)
+    assert stan_input["N_transport"] == 2, "Standard transport reaciton"
+    model_small.Exclude_list = ["g6p/g1p", "f6p/g6p"]
+    # Write the files again
+    write_model_files(model_small, test_dir)
+    stan_input = stan_input_from_dir(test_dir)
+    # Test the expected input
+    assert stan_input["N_transport"] == 4, "Expect extra transport reaction"
+
+
+@pytest.mark.usefixtures("model_small")
+def test_excluded_reactions_not_present(model_small):
+    # Add the test dir
+    test_dir = Path("test_dir")
+    # Test first without the new excluded reaction
+    stan_input = stan_input_from_dir(test_dir)
+    assert stan_input["N_transport"] == 2, "Standard transport reaciton"
+    model_small.Exclude_list = ["not_present"]
+    # Write the files again
+    write_model_files(model_small, test_dir)
+    stan_input = stan_input_from_dir(test_dir)
+    # Test the expected input
+    assert stan_input["N_transport"] == 2, "Expect extra transport reaction"
+
+
