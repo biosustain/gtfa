710d7020fbe623ad9add217c9e95889291614c39
Jason Jooste <jason.jooste@campus.lmu.de>
2022-01-20 15:38:00 +0100

    Introduce the skeleton for a reduced rank dgf cov matrix - doesn't work yet

--- tests/model_setup.py
+++ tests/model_setup.py
@@ -74,7 +74,33 @@ def model_small_rankdef():
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
     # Write the files
-    tmodel = build_small_test_model_rankdef()
+    tmodel = build_small_test_model_rankdef_stoich()
+    test_dir = Path("test_dir")
+    result_dir = test_dir / "results"
+    if test_dir.exists():
+        shutil.rmtree(test_dir)
+    test_dir.mkdir()
+    result_dir.mkdir()
+    if cc is None:
+        cc = ComponentContribution()
+    model_conversion.write_model_files(tmodel, test_dir, cc=cc)
+    # We need at least one measurment
+    header = pd.DataFrame(columns=["measurement_type","target_id","condition_id","measurement","error_scale"],
+                          data=[["mic", "f6p_c", "condition_1", 0.001, 0.1]])
+    header.to_csv(test_dir / "measurements.csv", index=False)
+    yield tmodel
+    # Clean up
+    shutil.rmtree(test_dir)
+
+@pytest.fixture
+def model_small_rankdef_thermo():
+    global cc
+    # Make sure that logging print statements still work
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    # Write the files
+    tmodel = build_small_test_model_rankdef_thermo()
     test_dir = Path("test_dir")
     result_dir = test_dir / "results"
     if test_dir.exists():
@@ -93,7 +119,8 @@ def model_small_rankdef():
     shutil.rmtree(test_dir)
 
 
-def build_small_test_model_rankdef():
+
+def build_small_test_model_rankdef_stoich():
     """ From the example from Metabolic control theory: A structural approach
     https://doi.org/10.1016/S0022-5193(88)80073-0
 
@@ -166,4 +193,46 @@ def build_small_test_model():
     model.metabolites[1].annotation = {"kegg.compound": "C00668"}
     model.metabolites[2].annotation = {"kegg.compound": "C05001"}
     model.metabolites[3].annotation = {"kegg.compound": "C00103"}
+    return model
+
+# This test case doesn't produce a reduced rank dgf covaraince matrix
+def build_small_test_model_rankdef_thermo():
+    """ From the example from Metabolic control theory: A structural approach
+    https://doi.org/10.1016/S0022-5193(88)80073-0
+
+    The exact metabolites are unimportant and the row rank deficiency is important
+    """
+    model = Model("small_toy")
+    model.add_metabolites([
+        Metabolite(id="fum_c", name="Fumarate", compartment="c"),
+        Metabolite(id="male_c", name="Maleate", compartment="c"),
+        Metabolite(id="2obut_c", name="2 Oxobutyrate", compartment="c"),
+        Metabolite(id="acac_c", name="ACetoacetate", compartment="c"),
+        Metabolite(id="ATP", name="ATP", compartment="c"),
+        Metabolite(id="ADP", name="ADP", compartment="c"),
+        Metabolite(id="pi", name="Orhtophosphate", compartment="c"),
+    ])
+    model.add_reactions([Reaction("1"),
+                         Reaction("2"),
+                         Reaction("3"),
+                         Reaction("4")
+                         ])
+    # Add the reactions
+    # Add the stoichiometry
+    model.reactions[0].build_reaction_from_string("fum_c <--> male_c")
+    model.reactions[1].build_reaction_from_string("2obut_c <--> acac_c")
+    model.reactions[2].build_reaction_from_string("fum_c + ATP <--> 2obut_c + ADP + pi")
+    model.reactions[3].build_reaction_from_string("acac_c + ATP <--> male_c + ADP + pi")
+    # Boundary reactions
+    model.add_boundary(model.metabolites.get_by_id("fum_c"), type="sink")
+    model.add_boundary(model.metabolites.get_by_id("male_c"), type="exchange")
+    # Make the compartment info
+    # Add the KEGG ids
+    model.metabolites[0].annotation = {"kegg.compound": "C00122"}
+    model.metabolites[1].annotation = {"kegg.compound": "C01384"}
+    model.metabolites[2].annotation = {"kegg.compound": "C00109"}
+    model.metabolites[3].annotation = {"kegg.compound": "C00164"}
+    model.metabolites[4].annotation = {"kegg.compound": "C00002"}
+    model.metabolites[5].annotation = {"kegg.compound": "C00008"}
+    model.metabolites[6 ].annotation = {"kegg.compound": "C00009"}
     return model
--- tests/test_fitting.py
+++ tests/test_fitting.py
@@ -8,7 +8,7 @@ import arviz as az
 from src.fitting import run_stan
 from src.model_configuration import load_model_configuration
 from src.pandas_to_cmdstanpy import get_coords
-from model_setup import model_small, model_small_rankdef
+from model_setup import model_small, model_small_rankdef, model_small_rankdef_thermo
 
 
 @pytest.mark.usefixtures("model_small")
@@ -46,3 +46,21 @@ def test_rank_deficient():
     # Check the steady state for each sample
     for i in range(df.shape[0]):
         assert pytest.approx(0, abs=1e-6) == S @ df.iloc[i, :].values, "All flux samples should be balanced"
+
+# THe test case doesn't actually produce a reduced rank covariance matrix
+# @pytest.mark.usefixtures("model_small_rankdef_thermo")
+# def test_rank_deficient():
+#     """Test fitting with a rank-deficient matrix"""
+#     config = load_model_configuration("test_small_prior.toml")
+#     S = pd.read_csv(config.data_folder / "stoichiometry.csv", index_col=0)
+#     measurements = pd.read_csv(config.data_folder / "measurements.csv")
+#     priors = pd.read_csv(config.data_folder / "priors.csv")
+#     # This will be cleaned up with the rest of the files
+#     config.result_dir = config.data_folder / "results"
+#     # Remove the results file
+#     mcmc = run_stan(config)
+#     data = az.from_cmdstanpy(mcmc, coords=get_coords(S, measurements, priors, config.order))
+#     df = data.posterior.flux.to_dataframe().unstack("flux_dim_0")
+#     # Check the steady state for each sample
+#     for i in range(df.shape[0]):
+#         assert pytest.approx(0, abs=1e-6) == S @ df.iloc[i, :].values, "All flux samples should be balanced"
